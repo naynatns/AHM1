29fd8ea9472e2a17aa970738788aacfaa4f74806
Hp <Hp@DESKTOP-MEHK842>
2023-05-30 16:01:51 +0530

    DAY1 programs

---
+++ JavaProject/src/com/tnsif/java/basics/FirstProgram.java
@@ -0,0 +1,11 @@
+package com.tnsif.java.basics;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome all the students for my session");
+
+	}
+
+}
---
+++ JavaProject/src/com/tnsif/java/basics/IdentifiersDemo.java
@@ -0,0 +1,26 @@
+package com.tnsif.java.basics;
+
+public class IdentifiersDemo {
+
+	public static void main(String[] args) {
+		
+		//variable declarations-invalid identifier
+		
+		int a=15;  //general
+		//int for=12;//keywords -invalid
+		int numberOne=15;  //camelcase
+				//int @numberthree=20;
+		
+		//valid identifiers
+		
+		int $number=12;
+		int _number1=12;
+				
+				
+				
+		
+		
+
+	}
+
+}
